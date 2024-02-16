db3b906949b1ea3b71a79222359c185515df0753
Riley <riley.gabrielson@tcnbroadcasting.com>
2024-02-15 23:34:21 -0700

    visibility system

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,23 @@ use rltk::{GameState, Rltk, RGB};
 use specs::prelude::*;
 
 mod components;
-pub use components::{Player, Position, Renderable};
+pub use components::{Player, Position, Renderable, Viewshed};
 mod map;
 pub use map::*;
 mod player;
 use player::*;
 mod rect;
-// pub use rect::Rect;
+pub use rect::Rect;
+mod visibility_system;
+use visibility_system::VisibilitySystem;
 
 pub const MIN_X: i32 = 0;
 pub const MAX_X: i32 = 79;
 pub const MIN_Y: i32 = 0;
 pub const MAX_Y: i32 = 49;
 
-pub const MAP_WIDTH: usize = (MAX_X as usize) + 1;
-pub const MAP_HEIGHT: usize = (MAX_Y as usize) + 1;
+pub const MAP_WIDTH: i32 = MAX_X + 1;
+pub const MAP_HEIGHT: i32 = MAX_Y + 1;
 
 struct State {
     entity_component_system: World,
@@ -24,6 +26,8 @@ struct State {
 
 impl State {
     fn run_systems(&mut self) {
+        let mut vis = VisibilitySystem {};
+        vis.run_now(&self.entity_component_system);
         self.entity_component_system.maintain();
     }
 }
@@ -35,8 +39,7 @@ impl GameState for State {
         player_input(self, context);
         self.run_systems();
 
-        let map = self.entity_component_system.fetch::<Vec<TileType>>();
-        draw_map(&map, context);
+        draw_map(&self.entity_component_system, context);
 
         let positions = self.entity_component_system.read_storage::<Position>();
         let renderables = self.entity_component_system.read_storage::<Renderable>();
@@ -59,18 +62,28 @@ fn main() -> rltk::BError {
     gs.entity_component_system.register::<Position>();
     gs.entity_component_system.register::<Renderable>();
     gs.entity_component_system.register::<Player>();
+    gs.entity_component_system.register::<Viewshed>();
 
-    gs.entity_component_system.insert(new_map());
+    let map = Map::new_map_rooms_and_corridors();
+    let (player_x, player_y) = map.rooms[0].center();
+    gs.entity_component_system.insert(map);
 
     gs.entity_component_system
         .create_entity()
-        .with(Position { x: 40, y: 25 })
+        .with(Position {
+            x: player_x,
+            y: player_y,
+        })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
         .with(Player {})
+        .with(Viewshed {
+            visible_tiles: Vec::new(),
+            range: 8,
+        })
         .build();
 
     rltk::main_loop(context, gs)
--- src/player.rs
+++ src/player.rs
@@ -1,40 +1,46 @@
-use super::{xy_index, Player, Position, State, TileType, MAX_X, MAX_Y, MIN_X, MIN_Y};
+use super::{Map, Player, Position, State, TileType, Viewshed};
 use rltk::{Rltk, VirtualKeyCode};
 use specs::prelude::*;
 use std::cmp::{max, min};
 
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let mut viewsheds = ecs.write_storage::<Viewshed>();
+    let map = ecs.fetch::<Map>();
 
-    let map = ecs.fetch::<Vec<TileType>>();
+    for (_player, pos, viewshed) in (&mut players, &mut positions, &mut viewsheds).join() {
+        let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map.tiles[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
 
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_index(pos.x + delta_x, pos.y + delta_y);
-        if map[destination_idx] != TileType::Wall {
-            pos.x = min(MAX_X, max(MIN_X, pos.x + delta_x));
-            pos.y = min(MAX_Y, max(MIN_Y, pos.y + delta_y));
+            // viewshed.dirty = true;
         }
     }
 }
 
-pub fn player_input(state: &mut State, ctx: &mut Rltk) {
+pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
     // Player movement
     match ctx.key {
         None => {} // Nothing happened
         Some(key) => match key {
-            VirtualKeyCode::Left | VirtualKeyCode::H => {
-                try_move_player(-1, 0, &mut state.entity_component_system)
+            VirtualKeyCode::Left | VirtualKeyCode::Numpad4 | VirtualKeyCode::H => {
+                try_move_player(-1, 0, &mut gs.entity_component_system)
             }
-            VirtualKeyCode::Right | VirtualKeyCode::L => {
-                try_move_player(1, 0, &mut state.entity_component_system)
+
+            VirtualKeyCode::Right | VirtualKeyCode::Numpad6 | VirtualKeyCode::L => {
+                try_move_player(1, 0, &mut gs.entity_component_system)
             }
-            VirtualKeyCode::Up | VirtualKeyCode::K => {
-                try_move_player(0, -1, &mut state.entity_component_system)
+
+            VirtualKeyCode::Up | VirtualKeyCode::Numpad8 | VirtualKeyCode::K => {
+                try_move_player(0, -1, &mut gs.entity_component_system)
             }
-            VirtualKeyCode::Down | VirtualKeyCode::J => {
-                try_move_player(0, 1, &mut state.entity_component_system)
+
+            VirtualKeyCode::Down | VirtualKeyCode::Numpad2 | VirtualKeyCode::J => {
+                try_move_player(0, 1, &mut gs.entity_component_system)
             }
+
             _ => {}
         },
     }
